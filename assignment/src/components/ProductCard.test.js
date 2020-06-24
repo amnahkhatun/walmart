028ea59bfeeaf51800896d53a257107002abf302
amnahkhatun <amna.khatun1605@gmail.com>
2020-06-24 12:59:16 +0530

    implemented snapshot testing using jest and enzyme

---
+++ assignment/src/components/ProductCard.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
+import ProductCard from "./ProductCard";
+import "../setupTest.js";
+
+describe("ProductCard", () => {
+  it('should render correctly in "debug" mode', () => {
+    const component = shallow(<ProductCard />);
+
+    expect(component).toMatchSnapshot();
+  });
+});
